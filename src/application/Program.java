a80275aeffa6cafb5722a6da31df632bb0a043ef
João Albuqerque <devjoao66@gmail.com>
2026-07-26 20:54:46 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import model.entities.Department;
-import model.entities.Seller;
+import entities.Department;
+import entities.Seller;
 
 import java.util.Date;
 
--- src/entities/Department.java
+++ src/entities/Department.java
@@ -1,4 +1,4 @@
-package model.entities;
+package entities;
 
 import java.io.Serializable;
 import java.util.Objects;
