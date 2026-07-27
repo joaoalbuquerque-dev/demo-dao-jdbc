6d0c52ae172b07eed04d88a021f22e4c9c1192e7
João Albuqerque <devjoao66@gmail.com>
2026-07-26 23:06:54 -0300

    findById Implementation

--- src/dao/DaoFactory.java
+++ src/dao/DaoFactory.java
@@ -1,10 +1,13 @@
 package dao;
 
 import dao.impl.SellerDaoJDBC;
+import db.DB;
+
+import java.sql.Connection;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
