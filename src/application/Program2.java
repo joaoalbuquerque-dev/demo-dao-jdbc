afcd9cf36eb9ea514fd35428639765b1a80e7e60
João Albuqerque <devjoao66@gmail.com>
2026-07-28 17:34:21 -0300

    DepartmentDaoJDBC and Program2

---
+++ src/application/Program2.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        sc.close();
+    }
+}
